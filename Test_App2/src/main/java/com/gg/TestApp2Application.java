905aa562c0126f95a9e4e73a5b6d9d3a1f1434a8
kdfasdf <tntls99@gmail.com>
2022-07-05 19:48:22 +0900

    commit

--- Test_App2/src/main/java/com/gg/TestApp2Application.java
+++ Test_App2/src/main/java/com/gg/TestApp2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApp2Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(TestApp2Application.class, args);
 		System.out.println("hi! Main..");
