66fc3be3a1e62d89b3897c01be61d42018056de9
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2022-11-08 20:29:09 +0900

    11-08-02

--- Test_App2/front/src/http-common.js
+++ Test_App2/front/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://49.50.164.178:8080",
     header:{
         "Content-type":"application/json",
     }
